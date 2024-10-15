4091fe0b74b5323df771cc74cb08d9b9b5318006
Michael Sarahan <msarahan@nvidia.com>
2024-10-15 12:30:18 -0500

    update python tests of telemetry scripts

--- tests/test_rapids-get-telemetry-trace-id.py
+++ tests/test_rapids-get-telemetry-trace-id.py
@@ -23,7 +23,7 @@ def test_rapids_get_traceparent():
         # this should raise, because OTEL_SERVICE_NAME isn't set
     try:
         result = subprocess.run(
-            [os.path.join(TOOLS_DIR, "rapids-get-telemetry-traceparent"), "my_job"],
+            [os.path.join(TOOLS_DIR, "rapids-get-telemetry-traceparent")],
             env={
                 "GITHUB_REPOSITORY": "rapidsai/gha-tools",
                 "GITHUB_RUN_ID": "1123123",
@@ -35,34 +35,34 @@ def test_rapids_get_traceparent():
     except subprocess.CalledProcessError:
         pass
     result = subprocess.run(
-        os.path.join(TOOLS_DIR, "rapids-get-telemetry-traceparent"),
+        [os.path.join(TOOLS_DIR, "rapids-get-telemetry-traceparent"), "my_job"],
         env={
             "GITHUB_REPOSITORY": "rapidsai/gha-tools",
             "GITHUB_RUN_ID": "1123123",
             "GITHUB_RUN_ATTEMPT": "1",
-            "OTEL_SERVICE_NAME": "my job"
-        },
-        text=True,
-        capture_output=True,
-    )
-    assert result.stdout.strip() == "00-22ab4ec60f37f446b4a95917e86660df-737c2f0e9d9bd9b0-01"
-    assert result.stderr == ""
-    assert result.returncode == 0
-
-def test_rapids_get_traceparent_with_step():
-    result = subprocess.run(
-        [os.path.join(TOOLS_DIR, "rapids-get-telemetry-traceparent"), "my step"],
-        env={
-            "GITHUB_REPOSITORY": "rapidsai/gha-tools",
-            "GITHUB_RUN_ID": "1123123",
-            "GITHUB_RUN_ATTEMPT": "1",
-            "OTEL_SERVICE_NAME": "my_job",
         },
         text=True,
         capture_output=True,
     )
     assert result.stdout.strip() == "00-22ab4ec60f37f446b4a95917e86660df-5f57388b5b07a3e8-01"
-    assert result.stderr == ""
+    assert result.stderr == """JOB_SPAN_ID pre-hash: \"22ab4ec60f37f446b4a95917e86660df-my_job\"
+STEP_SPAN_ID pre-hash: \"22ab4ec60f37f446b4a95917e86660df-my_job-\"\n"""
+    assert result.returncode == 0
+
+def test_rapids_get_traceparent_with_step():
+    result = subprocess.run(
+        [os.path.join(TOOLS_DIR, "rapids-get-telemetry-traceparent"), "my_job", "my step"],
+        env={
+            "GITHUB_REPOSITORY": "rapidsai/gha-tools",
+            "GITHUB_RUN_ID": "1123123",
+            "GITHUB_RUN_ATTEMPT": "1",
+        },
+        text=True,
+        capture_output=True,
+    )
+    assert result.stdout.strip() == "00-22ab4ec60f37f446b4a95917e86660df-a6e5bc57fad91889-01"
+    assert result.stderr == """JOB_SPAN_ID pre-hash: \"22ab4ec60f37f446b4a95917e86660df-my_job\"
+STEP_SPAN_ID pre-hash: \"22ab4ec60f37f446b4a95917e86660df-my_job-my step\"\n"""
     assert result.returncode == 0
 
 
